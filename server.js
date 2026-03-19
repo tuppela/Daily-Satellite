8ec259542a358744a77dbd533df854ca8cbc0754
tuppela <riku.tuppela@gmail.com>
2026-03-19 08:10:23 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -4,10 +4,23 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Try public folder first, fall back to root
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname)));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  // Try public/index.html first, then root index.html
+  const publicPath = path.join(__dirname, "public", "index.html");
+  const rootPath = path.join(__dirname, "index.html");
+  
+  const fs = require("fs");
+  if (fs.existsSync(publicPath)) {
+    res.sendFile(publicPath);
+  } else if (fs.existsSync(rootPath)) {
+    res.sendFile(rootPath);
+  } else {
+    res.status(404).send("index.html not found");
+  }
 });
 
 app.listen(PORT, () => {
